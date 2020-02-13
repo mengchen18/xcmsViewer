175e299f14a15600dc73b65a4de71faaebe48c79
Chen <mengchen18@gmail.com>
2020-02-13 09:09:03 +0100

    bug fix

--- xcmsViewerData/R/get_annotation.R
+++ xcmsViewerData/R/get_annotation.R
@@ -1,31 +1,31 @@
 #' Get annotation of masses
-#' @param what the annotation to get, the options are: \cr 
-#'    HMDB \cr 
-#'    MSDIAL_BioMSMS-Neg-PlaSMA \cr 
-#'    MSDIAL_BioMSMS-Pos-PlaSMA \cr 
-#'    MSDIAL_KI-GIAR_zic-HILIC_Pos_v0.90 \cr 
-#'    MSDIAL_Neg-CASMI2016 \cr 
-#'    MSDIAL_Neg-FiehnHILIC \cr 
-#'    MSDIAL_Neg-GNPS \cr 
-#'    MSDIAL_Neg-MassBankEU \cr 
-#'    MSDIAL_Neg-MassBank \cr 
-#'    MSDIAL_Neg-MetaboBASE \cr 
-#'    MSDIAL_Neg-PlaSMA \cr 
-#'    MSDIAL_Neg-Respect \cr 
-#'    MSDIAL_Neg-RikenOxPLs \cr 
-#'    MSDIAL_Neg-Vaniya-Fiehn_Natural_Products_Library_20200109 \cr 
-#'    MSDIAL_Pos-CASMI2016 \cr 
-#'    MSDIAL_Pos-FiehnHILIC \cr 
-#'    MSDIAL_Pos-GNPS \cr 
-#'    MSDIAL_Pos-MassBankEU \cr 
-#'    MSDIAL_Pos-MassBank \cr 
-#'    MSDIAL_Pos-MetaboBASE \cr 
-#'    MSDIAL_Pos-Pathogen_Box_20200109 \cr 
-#'    MSDIAL_Pos-PlaSMA \cr 
-#'    MSDIAL_Pos-Respect \cr 
-#'    MSDIAL_Pos-Vaniya-Fiehn_Natural_Products_Library_20200109 \cr 
-#'    MSDIAL_Public-Neg-VS14 \cr 
-#'    MSDIAL_Public-Pos-VS14 \cr 
+#' @param what the annotation to get, the options are:\cr 
+#'    HMDB\cr 
+#'    MSDIAL_BioMSMS-Neg-PlaSMA\cr 
+#'    MSDIAL_BioMSMS-Pos-PlaSMA\cr 
+#'    MSDIAL_KI-GIAR_zic-HILIC_Pos_v0.90\cr 
+#'    MSDIAL_Neg-CASMI2016\cr 
+#'    MSDIAL_Neg-FiehnHILIC\cr 
+#'    MSDIAL_Neg-GNPS\cr 
+#'    MSDIAL_Neg-MassBankEU\cr 
+#'    MSDIAL_Neg-MassBank\cr 
+#'    MSDIAL_Neg-MetaboBASE\cr 
+#'    MSDIAL_Neg-PlaSMA\cr 
+#'    MSDIAL_Neg-Respect\cr 
+#'    MSDIAL_Neg-RikenOxPLs\cr 
+#'    MSDIAL_Neg-Vaniya-Fiehn_Natural_Products_Library_20200109\cr 
+#'    MSDIAL_Pos-CASMI2016\cr 
+#'    MSDIAL_Pos-FiehnHILIC\cr 
+#'    MSDIAL_Pos-GNPS\cr 
+#'    MSDIAL_Pos-MassBankEU\cr 
+#'    MSDIAL_Pos-MassBank\cr 
+#'    MSDIAL_Pos-MetaboBASE\cr 
+#'    MSDIAL_Pos-Pathogen_Box_20200109\cr 
+#'    MSDIAL_Pos-PlaSMA\cr 
+#'    MSDIAL_Pos-Respect\cr 
+#'    MSDIAL_Pos-Vaniya-Fiehn_Natural_Products_Library_20200109\cr 
+#'    MSDIAL_Public-Neg-VS14\cr 
+#'    MSDIAL_Public-Pos-VS14\cr 
 #' @export
 #' 
 
@@ -70,34 +70,34 @@ get_annotation_mass <- function(what) {
 
 
 #' Get annotation of fragments (MS2)
-#' @param what the annotation to get, the options are: \cr 
-#'    HMDB_experimental \cr 
-#'    HMDB_predicted \cr 
-#'    MSDIAL_BioMSMS-Neg-PlaSMA \cr 
-#'    MSDIAL_BioMSMS-Pos-PlaSMA \cr 
-#'    MSDIAL_KI-GIAR_zic-HILIC_Pos_v0.90 \cr 
-#'    MSDIAL_Neg-CASMI2016 \cr 
-#'    MSDIAL_Neg-FiehnHILIC \cr 
-#'    MSDIAL_Neg-GNPS \cr 
-#'    MSDIAL_Neg-MassBankEU \cr 
-#'    MSDIAL_Neg-MassBank \cr 
-#'    MSDIAL_Neg-MetaboBASE \cr 
-#'    MSDIAL_Neg-PlaSMA \cr 
-#'    MSDIAL_Neg-Respect \cr 
-#'    MSDIAL_Neg-RikenOxPLs \cr 
-#'    MSDIAL_Neg-Vaniya-Fiehn_Natural_Products_Library_20200109 \cr 
-#'    MSDIAL_Pos-CASMI2016 \cr 
-#'    MSDIAL_Pos-FiehnHILIC \cr 
-#'    MSDIAL_Pos-GNPS \cr 
-#'    MSDIAL_Pos-MassBankEU \cr 
-#'    MSDIAL_Pos-MassBank \cr 
-#'    MSDIAL_Pos-MetaboBASE \cr 
-#'    MSDIAL_Pos-Pathogen_Box_20200109 \cr 
-#'    MSDIAL_Pos-PlaSMA \cr 
-#'    MSDIAL_Pos-Respect \cr 
-#'    MSDIAL_Pos-Vaniya-Fiehn_Natural_Products_Library_20200109 \cr 
-#'    MSDIAL_Public-Neg-VS14 \cr 
-#'    MSDIAL_Public-Pos-VS14 \cr 
+#' @param what the annotation to get, the options are:\cr 
+#'    HMDB_experimental\cr 
+#'    HMDB_predicted\cr 
+#'    MSDIAL_BioMSMS-Neg-PlaSMA\cr 
+#'    MSDIAL_BioMSMS-Pos-PlaSMA\cr 
+#'    MSDIAL_KI-GIAR_zic-HILIC_Pos_v0.90\cr 
+#'    MSDIAL_Neg-CASMI2016\cr 
+#'    MSDIAL_Neg-FiehnHILIC\cr 
+#'    MSDIAL_Neg-GNPS\cr 
+#'    MSDIAL_Neg-MassBankEU\cr 
+#'    MSDIAL_Neg-MassBank\cr 
+#'    MSDIAL_Neg-MetaboBASE\cr 
+#'    MSDIAL_Neg-PlaSMA\cr 
+#'    MSDIAL_Neg-Respect\cr 
+#'    MSDIAL_Neg-RikenOxPLs\cr 
+#'    MSDIAL_Neg-Vaniya-Fiehn_Natural_Products_Library_20200109\cr 
+#'    MSDIAL_Pos-CASMI2016\cr 
+#'    MSDIAL_Pos-FiehnHILIC\cr 
+#'    MSDIAL_Pos-GNPS\cr 
+#'    MSDIAL_Pos-MassBankEU\cr 
+#'    MSDIAL_Pos-MassBank\cr 
+#'    MSDIAL_Pos-MetaboBASE\cr 
+#'    MSDIAL_Pos-Pathogen_Box_20200109\cr 
+#'    MSDIAL_Pos-PlaSMA\cr 
+#'    MSDIAL_Pos-Respect\cr 
+#'    MSDIAL_Pos-Vaniya-Fiehn_Natural_Products_Library_20200109\cr 
+#'    MSDIAL_Public-Neg-VS14\cr 
+#'    MSDIAL_Public-Pos-VS14\cr 
 #' @export
 
 get_annotation_fragment <- function(what) {
@@ -137,5 +137,5 @@ get_annotation_fragment <- function(what) {
   dl <- lapply(what, function(w) {
     readRDS(file.path(dir, paste0(w, ".rds")))
   })
-  combine_ms2(dl)
+  combine_ms2(list=dl)
 }
